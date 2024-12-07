f28269b30e6efedb3cca6369e114ce91600dcf15
KyeongBin Min <min06150315@gmail.com>
2024-12-08 01:27:14 +0900

    Title: 출석 부르기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241208/출석 부르기/calling-attendance.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String res;
+
+        if (a == 1) {
+            res = "John";
+        } else if (a == 2) {
+            res = "Tom";
+        } else if (a == 3) {
+            res = "Paul";
+        } else {
+            res = "Vancancy";
+        }
+        System.out.println(res);
+    }
+}
