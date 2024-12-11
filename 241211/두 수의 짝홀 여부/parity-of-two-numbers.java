96a91a166a3d73e01b44f6315ad860e060f670a5
KyeongBin Min <min06150315@gmail.com>
2024-12-11 23:06:34 +0900

    Title: 두 수의 짝홀 여부, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241211/두 수의 짝홀 여부/parity-of-two-numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String result;
+        
+        if (a % 2 == 0) {
+            result = "even";
+        } else {
+            result = "odd";
+        }
+        System.out.println(result);
+        
+        if (b % 2 == 0) {
+            result = "even";
+        } else {
+            result = "odd";
+        }
+        System.out.println(result);
+    }
+}
