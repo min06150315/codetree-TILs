73217e4427384c67f265bcd692e6a41f44da6b0f
KyeongBin Min <min06150315@gmail.com>
2024-12-13 01:29:11 +0900

    Title: 범위 안의 수, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241213/범위 안의 수/number-in-range.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String result;
+        
+        if (a >= 10 && a <= 20) {
+            result = "yes";
+        } else {
+            result = "no";
+        }
+        System.out.println(result);
+    }
+}
