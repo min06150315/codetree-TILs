927d2ed767181ffe65a1a7255fbd36d81360e493
KyeongBin Min <min06150315@gmail.com>
2024-12-13 01:39:39 +0900

    Title: 특정 조건 세 정수 비교, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 241213/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 241213/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -4,32 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        int result;
-        int min = a;
 
-        if (b < min) {
-            min = b;
-        } 
+        int min = Math.min(a, Math.min(b, c));
 
-        if (c < min) {
-            min = c;
-        }
-        
-        if (a == min) {
-            result = 1;
-        } else {
-            result = 0;
-        }
-        System.out.print(result + " ");
-
-        if ((a == b) && (a == c)) {
-            result = 1;
-        } else {
-            result = 0;
-        }
-        System.out.println(result);
+        System.out.print((a == min ? 1 : 0 ) + " ");
+        System.out.println((a == b && b == c ? 1 : 0 ));   
     }
 }
