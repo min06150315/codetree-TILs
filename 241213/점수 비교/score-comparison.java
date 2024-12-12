a6a9dc4261b747fc38488cf459d6378f877f7359
KyeongBin Min <min06150315@gmail.com>
2024-12-13 01:21:55 +0900

    Title: 점수 비교, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241213/점수 비교/score-comparison.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a_math = sc.nextInt();
+        int b_math = sc.nextInt();
+        int a_eng = sc.nextInt();
+        int b_eng = sc.nextInt();
+        int result;
+        
+        if (a_math > b_math && a_eng > b_eng) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        System.out.println(result);
+    }
+}
