6151fcc4e7cee3c02639111660e4b699962057be
KyeongBin Min <min06150315@gmail.com>
2024-12-14 09:56:53 +0900

    Title: 굉장한 수, Time: 129ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 241214/굉장한 수/amazing-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String res;
+
+        if ((n % 2 == 1 || n % 3 == 0) || (n % 2 == 0 || n % 5 == 0)) {
+            res = "true";
+        } else {
+            res = "false";
+        }
+
+        System.out.println(res); 
+    }
+}
